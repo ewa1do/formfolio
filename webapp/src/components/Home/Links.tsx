54725d9463c0aa4b5c329eac15b689fabc974eb3
ewa1do <this.eduardovera@gmail.com>
2023-07-21 18:40:23 -0400

    adding notification within Links component when button is clicked

--- webapp/src/components/Home/Links.tsx
+++ webapp/src/components/Home/Links.tsx
@@ -1,34 +1,36 @@
+import { MouseEvent, useContext } from 'react'
+import { PiSuitcaseSimple } from 'react-icons/pi'
 import {
     IoLogoLinkedin,
     IoLogoTwitter,
     IoLogoGithub,
-    IoCloudSharp,
     IoLogoBehance,
+    IoDocumentOutline,
 } from 'react-icons/io5'
-import { PiSuitcaseSimple } from 'react-icons/pi'
-import { MouseEvent, useContext } from 'react'
-import { LOCALSTORAGE_KEYS, UserLinks } from '../../models'
-import Utils from '../../utils'
-import { Title } from '../UI/Title'
 import { UserContext } from '../../contexts/userContext'
+import { Title } from '../UI/Title'
+import { Notification } from '../UI/Notification'
+import { useNotification } from '../../hooks/useNotification'
+import { LOCALSTORAGE_KEYS, UserLinks } from '../../models'
+import utils from '../../utils'
 
 export function Links() {
-    const links = Utils.getFields<UserLinks>(LOCALSTORAGE_KEYS.LINK)
-
-    const { isModeChanged } = useContext(UserContext)
+    const links = utils.getFields<UserLinks>(LOCALSTORAGE_KEYS.LINK)
+    const { isModeChanged, selectedLang } = useContext(UserContext)
+    const { showNotification, triggerNotification } = useNotification()
+    const { notification } = utils.getAppTexts()
 
     async function getMetadataAndCopyToClipboard(
         e: MouseEvent<HTMLButtonElement>
     ) {
         const target = e.target as HTMLButtonElement
 
-        const linkbtn = target.closest('button')
+        const linkbtn = target.closest('button') as HTMLButtonElement
         const metadata = linkbtn?.getAttribute('data-link') as string
 
         try {
             await navigator.clipboard.writeText(metadata)
-
-            console.log('Text copied to clipboard')
+            triggerNotification()
         } catch (error) {
             console.log(error)
         }
@@ -38,35 +40,42 @@ export function Links() {
         linkedin: <IoLogoLinkedin />,
         twitter: <IoLogoTwitter />,
         portfolio: <PiSuitcaseSimple />,
-        resume: <IoCloudSharp />,
+        resume: <IoDocumentOutline />,
         github: <IoLogoGithub />,
         behance: <IoLogoBehance />,
     }
 
     return (
-        <section>
-            <Title text="Links" />
-            <ul className="flex">
-                {Object.entries(links).map(([value, link]) => {
-                    return (
-                        <li
-                            key={`link-${value}`}
-                            className={`text-2xl mr-1 md:mr-3 border-2 p-1 rounded-md flex ${
-                                isModeChanged
-                                    ? 'border-aqua-100'
-                                    : 'border-aqua-50'
-                            } `}
-                        >
-                            <button
-                                data-link={link}
-                                onClick={getMetadataAndCopyToClipboard}
+        <>
+            <section>
+                <Title text="Social" />
+                <ul className="flex">
+                    {Object.entries(links).map(([value, link]) => {
+                        return (
+                            <li
+                                key={`link-${value}`}
+                                className={`text-2xl mr-1 md:mr-3 border-2 p-1 rounded-md flex ${
+                                    isModeChanged
+                                        ? 'border-aqua-100'
+                                        : 'border-aqua-50'
+                                } `}
                             >
-                                {iconsMap[value]}
-                            </button>
-                        </li>
-                    )
-                })}
-            </ul>
-        </section>
+                                <button
+                                    data-link={link}
+                                    onClick={getMetadataAndCopyToClipboard}
+                                >
+                                    {iconsMap[value]}
+                                </button>
+                                {showNotification && (
+                                    <Notification
+                                        message={notification[selectedLang]}
+                                    />
+                                )}
+                            </li>
+                        )
+                    })}
+                </ul>
+            </section>
+        </>
     )
 }
